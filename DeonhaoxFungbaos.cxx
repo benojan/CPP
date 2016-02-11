67b4b4155fd7df0e5961fc84f5aafb52535c2071
Benojan <skoj@qq.com>
2016-02-11 20:39:07 +0800

    Update DeonhaoxFungbaos.cxx

--- DeonhaoxFungbaos.cxx
+++ DeonhaoxFungbaos.cxx
@@ -27,3 +27,30 @@ int main(int argc, char* argv[])
     //wout << ws << endl;
     //wout.close();
 }
+
+
+#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <fstream>
+#include <cstdio>
+#include <exception>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    //cout << loc.name() << endl;
+    //wstring ws = L"\x4F60\x597D\x554A";//"\x4F60\x597D\x554A";
+    //wcout << ws << endl;
+  char ch1 = 't';
+  wchar_t ch2 = L'\x4F60'; // 最好用unicode编码的十六进制表示汉字！
+  cout << hex << (int) ch1 << " " << ch2 << endl; //output:74 4f60------即输出成功
+  wcout << hex << (int) ch1 << L" " << ch2 << endl; //output:74 --------前面输出成功，后面输出失败
+
+    //printf("%x", ws[0]);
+    //wcout << hex << ws[0] << endl;
+    //string fileName = "C:\\Users\\Administrator\\Desktop\\hello.txt";
+    //wofstream wout(fileName);
+    //wout << ws << endl;
+    //wout.close();
+}
