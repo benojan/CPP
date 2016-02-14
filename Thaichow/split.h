c4515145611c197bf3b5aa323ed40a5745a4011a
Benojan <skoj@qq.com>
2016-02-14 22:50:53 +0800

    Delete split.h

--- Thaichow/split.h
+++
@@ -1,9 +0,0 @@
-#ifndef __beno_split__
-#define __beno_split__
-#include <iostream>
-#include <vector>
-using namespace std;
-
-void split(const wstring& src, const wstring& sep, vector<wstring>& des);
-
-#endif
