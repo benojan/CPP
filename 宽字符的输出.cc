66750ab9db9b377e689fbe956c912b758db90780
Benojan <skoj@qq.com>
2021-11-23 19:56:20 +0800

    Create 宽字符的输出.cc

---
+++ 宽字符的输出.cc
@@ -0,0 +1,14 @@
+/**
+* 功能：输出wstring到命令行。
+*/
+
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	wstring ws = L"快活险"; //\x5FEB\x6D3B\x9669
+	wofstream out("output.txt");
+	out.imbue(locale(""));
+	out << ws << endl;
+}
