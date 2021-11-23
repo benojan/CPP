70314c21e13eb91b924ae2ca4f0adfb6fd421b5c
Benojan <skoj@qq.com>
2021-11-23 20:13:33 +0800

    Update 宽字符的输出.cc

--- code/宽字符的输出.cc
+++ code/宽字符的输出.cc
@@ -12,3 +12,20 @@ int main()
 	out.imbue(locale(""));
 	out << ws << endl;
 }
+
+//=================================================
+
+/**
+* 功能：输出wstring到utf8文本文件。
+*/
+
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	wstring ws = L"快活险"; //\x5FEB\x6D3B\x9669
+	wofstream out("output.txt");
+	out.imbue(locale("zh_CN.utf8"));
+	out << ws << endl;
+}
